b461007a5c10423133d448fad22d9efddfdb19e0
Severle <2096545154@qq.com>
2024-01-18 01:50:35 +0800

    退出提示对话框

---
+++ TouHou_Getaway/src/main/java/org/thcg/ui/dialog/QuitConfirmDialog.kt
@@ -0,0 +1,11 @@
+package org.thcg.ui.dialog
+
+import javax.swing.JDialog
+
+/**
+ * @author Severle
+ * @date 2024-01-18 00:30:13
+ * @description 退出确认对话框
+ */
+class QuitConfirmDialog : JDialog() {
+}
