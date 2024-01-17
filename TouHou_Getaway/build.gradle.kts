5661250168db9c886e67ba1b95cfe6b9d53e3f9e
Severle <2096545154@qq.com>
2024-01-18 00:05:10 +0800

    引入lwjgl,修改kotlin协程版本

--- TouHou_Getaway/build.gradle.kts
+++ TouHou_Getaway/build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
     mavenCentral()
 }
 
+val lwjglVersion = "3.3.3"
 val log4jVersion = "3.0.0-beta1"
 val gsonVersion = "2.10.1"
 val xmlVersion = "4.0.1"
@@ -22,10 +23,36 @@ val migLayoutVersion = "11.2"
 val flatlafVersion = "3.2.5"
 val flatlafJetbrainsMonoVersion = "2.242"
 val kotlinVersion = "2.0.0-Beta2"
-val kotlinCoroutinesVersion = "1.8.0-RC2"
+val kotlinCoroutinesVersion = "1.7.1"
 val lombokVersion = "1.18.30"
 val junitVersion = "5.9.2"
 val kotlinTestVersion = "1.8.10"
+
+val lwjglNatives = Pair(
+    System.getProperty("os.name")!!,
+    System.getProperty("os.arch")!!
+).let { (name, arch) ->
+    when {
+        arrayOf("Linux", "SunOS", "Unit").any { name.startsWith(it) } ->
+            if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
+                "natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
+            else if (arch.startsWith("ppc"))
+                "natives-linux-ppc64le"
+            else if (arch.startsWith("riscv"))
+                "natives-linux-riscv64"
+            else
+                "natives-linux"
+        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) }     ->
+            "natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+        arrayOf("Windows").any { name.startsWith(it) }                ->
+            if (arch.contains("64"))
+                "natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+            else
+                "natives-windows-x86"
+        else                                                                            ->
+            throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
+    }
+}
 
 dependencies {
     implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
@@ -40,11 +67,31 @@ dependencies {
     implementation("com.formdev:flatlaf-extras:$flatlafVersion")
     implementation("com.formdev:flatlaf-fonts-jetbrains-mono:$flatlafJetbrainsMonoVersion")
 
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-freetype")
+    implementation("org.lwjgl", "lwjgl-jemalloc")
+    implementation("org.lwjgl", "lwjgl-lz4")
+    implementation("org.lwjgl", "lwjgl-nfd")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-stb")
+    implementation("org.lwjgl", "lwjgl-xxhash")
+    implementation("org.lwjgl", "lwjgl-zstd")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-freetype", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-jemalloc", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-lz4", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-nfd", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-xxhash", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-zstd", classifier = lwjglNatives)
+
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jvm:$kotlinCoroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:$kotlinCoroutinesVersion")
 
     compileOnly("org.projectlombok:lombok:$lombokVersion")
