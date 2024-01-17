c900228ea8559bf296382a6f8b78f7a7a10fa3de
Severle <2096545154@qq.com>
2024-01-18 01:49:59 +0800

    类文件初始化

--- TouHou_Getaway/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击间距中的 <icon src="AllIcons.Actions.Execute"/> 图标。
-public class Main {
-    public static void main(String[] args) {
-        new gamepage().create("Touhou Getaway");
-    }
-}
