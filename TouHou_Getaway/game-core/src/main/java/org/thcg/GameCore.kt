7deab1bac16aff46565dfe6baa25165851342fb9
纯水子 <98969591+chunshuizi@users.noreply.github.com>
2024-01-18 22:21:46 +0800

    更换库为libGDX,增加自机参考底层逻辑

--- TouHou_Getaway/game-core/src/main/java/org/thcg/GameCore.kt
+++ TouHou_Getaway/game-core/src/main/java/org/thcg/GameCore.kt
@@ -1,32 +1,32 @@
 package org.thcg
 
 import com.badlogic.gdx.ApplicationAdapter
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.utils.ScreenUtils
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 
-/**
- * @author Severle
- * @date 2024-01-18 15:46:47
- * @description
- */
 class GameCore : ApplicationAdapter() {
-    private lateinit var batch: SpriteBatch
-    private lateinit var img: Texture
+    var x: Int = 50
+    var y: Int = 50
+    var xSpeed: Int = 0
+    var shape: ShapeRenderer? = null
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
-    }
+        shape = ShapeRenderer()
 
+    }
     override fun render() {
-        ScreenUtils.clear(1f, 0f, 0f, 1f)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
+        x += 5
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        shape!!.begin(ShapeRenderer.ShapeType.Filled)
+        shape!!.circle(x.toFloat(), y.toFloat(), 10f)
+        shape!!.end()
+        x += xSpeed;
+        if (x > Gdx.graphics.getWidth()) {
+            xSpeed = -10;
+        }
+        if (x < 0) {
+            xSpeed = 0;
+        }
     }
 
-    override fun dispose() {
-        batch.dispose()
-        img.dispose()
-    }
 }
