26129fcbcecb4ffeef5041beaf913b36ff869b9d
纯水子 <xzsjy123456@163.com>
2024-01-19 14:48:27 +0800

    使自机强制限制在界面当中，后期需修改界面位置。自机变成红色（笑）

--- TouHou_Getaway/game-core/src/main/java/org/thcg/GameCore.kt
+++ TouHou_Getaway/game-core/src/main/java/org/thcg/GameCore.kt
@@ -2,6 +2,7 @@ package org.thcg
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import org.thcg.input.MyInputProcessor
@@ -12,7 +13,7 @@ class GameCore : ApplicationAdapter() {
     private lateinit var inputProcessor: MyInputProcessor
 
     private var x: Int = 300
-    private var y: Int = 5
+    private var y: Int = 8
     private var xSpeed: Int = 0
     private var ySpeed: Int = 0
     private lateinit var shape: ShapeRenderer
@@ -26,22 +27,24 @@ class GameCore : ApplicationAdapter() {
     override fun render() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         shape.begin(ShapeRenderer.ShapeType.Filled)
-        shape.circle(x.toFloat(), y.toFloat(), 10f)
+        shape.circle(x.toFloat(), y.toFloat(), 8f)
+        val color = Color.RED
+        shape.color=color
         shape.end()
         x += xSpeed
         y += ySpeed
 
-        if (x >= Gdx.graphics.getWidth()) {
-            x = Gdx.graphics.getWidth()
+        if (x >= Gdx.graphics.getWidth()-8) {
+            x = Gdx.graphics.getWidth()-8
         }
-        if (x <= 0) {
-            x = 0
+        if (x <= 8) {
+            x = 8
         }
-        if (y >= Gdx.graphics.getWidth()) {
-            y = Gdx.graphics.getWidth()
+        if (y >= Gdx.graphics.getWidth()-168) {
+            y = Gdx.graphics.getWidth()-168
         }
-        if (y <= 0) {
-            y = 0
+        if (y <= 8) {
+            y = 8
         }
 
     }
