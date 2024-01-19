80f2e5e1b34719adfae5f13d94b0087ee0984edf
Severle <2096545154@qq.com>
2024-01-19 15:37:31 +0800

    修改资源加载类

---
+++ TouHou_Getaway/game-core/src/main/java/org/thcg/screen/MainScreen.kt
@@ -0,0 +1,14 @@
+package org.thcg.screen
+
+import de.eskalon.commons.screen.ManagedScreenAdapter
+
+/**
+ * @author Severle
+ * @date 2024-01-19 15:31:00
+ * @description
+ */
+class MainScreen : ManagedScreenAdapter() {
+    override fun render(delta: Float) {
+        super.render(delta)
+    }
+}
