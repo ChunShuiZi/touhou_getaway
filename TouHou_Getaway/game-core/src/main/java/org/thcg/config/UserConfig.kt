ec42f0ee67de01b6559a68ab8b42c413632b1742
Severle <2096545154@qq.com>
2024-01-22 16:27:41 +0800

    更新文件序列化

--- TouHou_Getaway/game-core/src/main/java/org/thcg/config/UserConfig.kt
+++ TouHou_Getaway/game-core/src/main/java/org/thcg/config/UserConfig.kt
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.thcg.util.CanSerializable
 import org.thcg.util.Resources
-import java.io.OutputStream
 
 /**
  * @author Severle
@@ -16,17 +15,6 @@ data class UserConfig(
 ) : CanSerializable {
     companion object {
         private val log: Logger = LogManager.getLogger(UserConfig::class.java)
-    }
-    override fun serialize(os: OutputStream): Boolean {
-        try {
-            Resources.serialize(os, this)
-            return true
-        } catch (e: Exception) {
-            if (log.isWarnEnabled) {
-                log.warn("Exception caught when serialize config, {}", e.localizedMessage, e)
-            }
-            return false
-        }
     }
 
     override fun type(): Resources.SerializeType {
--- TouHou_Getaway/game-core/src/main/java/org/thcg/util/CanSerializable.java
+++ TouHou_Getaway/game-core/src/main/java/org/thcg/util/CanSerializable.java
@@ -1,16 +1,11 @@
 package org.thcg.util;
 
-import lombok.NonNull;
-
-import java.io.OutputStream;
-
 /**
  * @author Severle
  * @date 2024-01-22 15:58:16
  * @description
  */
 public interface CanSerializable {
-    boolean serialize(@NonNull OutputStream os);
 
     Resources.SerializeType type();
 }
